77c38dde84cf2532c30e5687bad2a834b53f141d
Sarahsegla <sarahsegla@icloud.com>
2023-09-04 12:31:31 +0100

    navbar and jumbo blend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'whitiesh': 'rgba(237, 238, 188, 1)',
+      },
+    },
   },
   plugins: [],
 }
